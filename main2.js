bbeea99d2c1cfdc8f5cfa83ead7a5ec1926d930d
Ryan <asdfasdf>
2022-12-30 23:57:42 +0800

    finished

--- main2.js
+++ main2.js
@@ -20,12 +20,51 @@ class Calculator {
     this.currentOperand = this.currentOperand.toString() + number.toString()
   }
 
-  chooseOperation(operation) {}
-
-  compute() {} 
+  chooseOperation(operation) {
+    if(this.currentOperand==="")return
+    if(this.previousOperand!==""){
+      this.compute()
+    }
+    this.operation=operation
+    this.previousOperand=this.currentOperand
+    this.currentOperand=""
+  }
+ 
+  compute() {
+    let computation
+    const prev=parseFloat(this.previousOperand)
+    const current=parseFloat(this.currentOperand)
+    if(isNaN(prev)||isNaN(current))return 
+    switch(this.operation){
+      case "+":
+        computation=prev+current
+        break
+        case "-":
+          computation=prev-current
+          break
+          case "*":
+            computation=prev*current
+            break
+            case "÷":
+            computation=prev/current
+            break
+            default:
+              return
+    }
+    this.currentOperand=computation
+    this.operation=undefined
+    this.previousOperand=""
+  } 
 
   updateDisplay() {
    this.currentOperandTextElement.innerText = this.currentOperand;
+   if(this.operation!=null){
+    this.previousOperandTextElement.innerText=
+    `${this.previousOperand} ${this.operation}`
+   }
+   else{
+    this.previousOperandTextElement.innerText=""
+   }
   }
 }
 
@@ -48,7 +87,29 @@ const calculator = new Calculator(
 
 numberButtons.forEach(button => {
   button.addEventListener("click", () => {
-    calculator.appendNumber(button.innerHTML);
+    calculator.appendNumber(button.innerText);
     calculator.updateDisplay();
   });
 });
+
+operationButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton?.addEventListener("click",button=>{
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+clearButton?.addEventListener("click",button=>{
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton?.addEventListener("click",button=>{
+  calculator.delete()
+  calculator.updateDisplay()
+})
